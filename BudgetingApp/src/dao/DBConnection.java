32e4b44c59d47464cc6e47653aabc88a6400256a
william.hitchcock <william.hitchcock@B106-19.soit.local>
2019-07-31 16:21:13 +1200

    Setup mysql connection and executed a select statement.

--- BudgetingApp/src/dao/DBConnection.java
+++ BudgetingApp/src/dao/DBConnection.java
@@ -1,5 +1,34 @@
 package dao;
 
+import java.sql.*;
+
 public class DBConnection {
-	// database connection
+	public static void main(String[] args) {
+		String dbUrl = "jdbc:mysql://localhost:3306/users";
+		String user = "root";
+		String password = "";
+		
+		// establish the db connection
+		try {
+			Connection myConn = DriverManager.getConnection(dbUrl, user, password);
+			
+			// create a statement
+			Statement myStmt = myConn.createStatement();
+			
+			// execute the query
+			ResultSet rs = myStmt.executeQuery("select * from students");
+			
+			// process the resultSet object
+			while(rs.next()) {
+				System.out.println(rs.getString("username") + " " + rs.getString("password"));
+			}
+			
+			myStmt.close();
+			myConn.close();
+					
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
 }
